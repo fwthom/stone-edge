0f5ba81a3441d63d7c1491c3e4fd8247d663638c
Camille GUILMAIN <caguilmain@hotmail.fr>
2024-11-19 11:55:26 +0100

    test

--- app/controllers/stones_controller.rb
+++ app/controllers/stones_controller.rb
@@ -7,20 +7,6 @@ class StonesController < ApplicationController
     @stone = Stone.find(params[:id])
   end
 
-  def new
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
 
   def stone_params
     params.require(:stone).permit(:name, :backstory, :daily_price, :personality_traits, :condition, :size, :image_url, :user_id, :category_id)
