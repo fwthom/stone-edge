04b18b86b8f17e0dee4eda0564ecdb621cd7fbfa
Camille GUILMAIN <caguilmain@hotmail.fr>
2024-11-19 11:58:27 +0100

    new create

--- app/controllers/stones_controller.rb
+++ app/controllers/stones_controller.rb
@@ -7,6 +7,28 @@ class StonesController < ApplicationController
     @stone = Stone.find(params[:id])
   end
 
+  def new
+    @stone = Stone.new
+  end
+
+  def create
+    @stone = Stone.new(stone_params)
+
+    if @stone.save
+      redirect_to @stone, notice: 'Stone was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def stone_params
+    params.require(:stone).permit(:name, :backstory, :daily_price,
+      :personnality_traits, :condition, :size, :image_url, :user_id,
+      :category_id, :created_at, :updated_at)
+  end
+
 
   def stone_params
     params.require(:stone).permit(:name, :backstory, :daily_price, :personality_traits, :condition, :size, :image_url, :user_id, :category_id)
