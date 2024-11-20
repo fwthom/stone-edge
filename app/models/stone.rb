25df1ce72d29447fb8788616388ab7350f0ec9ac
Camille GUILMAIN <caguilmain@hotmail.fr>
2024-11-20 11:52:22 +0100

    en cours

--- app/models/stone.rb
+++ app/models/stone.rb
@@ -4,4 +4,14 @@ class Stone < ApplicationRecord
   has_one_attached :photo
   validates_presence_of :name
   has_many :bookings, dependent: :destroy
+
+  geocoded_by ->(obj) { "#{obj.user.city}, #{obj.user.country}" }
+  after_validation :geocode, if: :user_location_change?
+
+  private
+
+  # Vérifie si la ville ou le pays de l'utilisateur ont changé
+  def user_location_changed?
+    user&.saved_change_to_city? || user&.saved_change_to_country?
+  end
 end
