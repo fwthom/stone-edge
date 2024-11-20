1714384515afea4e50afb677a0e529d77436a776
Jean Delarbre <delarbrejean@icloud.com>
2024-11-20 12:25:45 +0100

    calcul auto prix

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="booking"
 export default class extends Controller {
-  static targets = ["startDate", "endDate", "price"]
+  static targets = ["startDate", "endDate", "price", "priceShow"]
   static values = { 
     startDate: String,
     endDate: String,
@@ -11,21 +11,31 @@ export default class extends Controller {
 
 
   connect() {
+    if (this.startDateValue && this.endDateValue) 
+      {
+        this.startDateTarget.value = this.startDateValue;
+          this.endDateTarget.value = this.endDateValue;
+          const date1 = new Date(this.startDateValue);
+          const date2 = new Date(this.endDateValue);
+          const priceString = this.priceValue;
+          const duration = Math.floor((date2 - date1) / (1000 * 60 * 60 * 24));
+          const price = parseFloat(priceString);
+          const totalPrice = duration * price;
+          this.priceShowTarget.innerText = `Le prix de la location est de ${totalPrice.toFixed(2)} €`
+        }
+  }
 
-    console.log(this.priceValue);
-    
+
+  updatePrice() {
+    console.log("update")
     this.startDateTarget.value = this.startDateValue;
     this.endDateTarget.value = this.endDateValue;
-
-    const dateString1 = this.startDateValue;
-    const date1 = new Date(dateString1);
-    const dateString2 = this.endDateValue;
-    const date2 = new Date(dateString2);
-
+    const date1 = new Date(this.startDateValue);
+    const date2 = new Date(this.endDateValue);
+    const priceString = this.priceValue;
     const duration = Math.floor((date2 - date1) / (1000 * 60 * 60 * 24));
-    const totalPrice = duration * this.priceValue;
-
-    console.log(totalPrice);
-    
+    const price = parseFloat(priceString);
+    const totalPrice = duration * price;
+    this.priceShowTarget.innerText = `Le prix de la location est de ${totalPrice.toFixed(2)} €`
   }
 }
