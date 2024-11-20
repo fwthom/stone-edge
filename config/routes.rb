bcf546fec7639108fd01f179c79f32af0fc9f2d1
Jean Delarbre <delarbrejean@icloud.com>
2024-11-20 09:44:26 +0100

    convergence vers new master

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,32 @@
 Rails.application.routes.draw do
   devise_for :users
   # Devise routes for users with custom sessions controller
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
 
   # Root path
 
   root to: "pages#home"
 
+  # get '/dashboard', to: 'pages#dashboard'
+
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Health check route
   get "up", to: "rails/health#show", as: :rails_health_check
 
-  # Nested user routes for bookings management
   resources :users do
     resources :bookings, only: [:index, :show, :edit, :update, :destroy]
+    member do
+      get :dashboard
+    end
   end
+
 
   # Stone routes with nested bookings
   resources :stones do
     resources :bookings, only: [:new, :create, :show]
   end
+  resources :pages, only: [] do
+    get 'profile/:id', to: 'pages#profile', as: :profile
+  end
 end
-  
