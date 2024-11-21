fab1a095dac3a6a21aa51b0482ad46e70c3b8b62
Thomas Barbieux <thomas@fresh.win>
2024-11-21 12:17:55 +0100

    Navbar open/close sur scroll + Homebanner done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_20_200720) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_21_101618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_20_200720) do
     t.text "properties"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "stones", force: :cascade do |t|
